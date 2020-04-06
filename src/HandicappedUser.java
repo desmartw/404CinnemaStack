18e0d74abe6bc59e576b5f0783375faa217bb029
Wils DesMarteau <wilsdesmarteau@Wilss-MacBook-Pro.local>
2020-04-05 22:02:44 -0400

    Adding Handicapped and student user subclasses

---
+++ src/HandicappedUser.java
@@ -0,0 +1,34 @@
+
+public class HandicappedUser extends User {
+	public String handicappedTagNum;
+	
+	public HandicappedUser() {
+		username = "handicappedBob123";
+		password = "password";
+		email = "handicappedBob123@email.com";
+		wallet = new Currency[3];
+		tickets = new ArrayList<Ticket>();
+		cart =  new ArrayList<Ticket>();
+		age = 18;
+		rewardPoints = 0;
+		discountRate = .1;
+		handicappedTagNum = "123CantWalk";
+	}
+	
+	public HandicappedUser(String username, String password, String email,
+			Currency[] wallet, ArrayList<Ticket> tickets,
+			ArrayList<Ticket> cart, int age, int rewardPoints, 
+			double discountRate, String handicappedTagNum) {
+		this.username = username;
+		this.password = password;
+		this.email = email;
+		this.wallet = wallet;
+		this.tickets = tickets;
+		this.cart =  cart;
+		this.age = age;
+		this.rewardPoints = rewardPoints;
+		this.discountRate = discountRate;
+		this.handicappedTagNum = handicappedTagNum;
+	}
+
+}
---
+++ src/StudentUser.java
@@ -0,0 +1,34 @@
+
+public class StudentUser extends User {
+	public String studentIDNum;
+	
+	public StudentUser() {
+		username = "studentBob123";
+		password = "password";
+		email = "studentBob123@email.com";
+		wallet = new Currency[3];
+		tickets = new ArrayList<Ticket>();
+		cart =  new ArrayList<Ticket>();
+		age = 18;
+		rewardPoints = 0;
+		discountRate = .1;
+		studentIDNum= "123LoveToStudy";
+	}
+	
+	public StudentUser(String username, String password, String email,
+			Currency[] wallet, ArrayList<Ticket> tickets,
+			ArrayList<Ticket> cart, int age, int rewardPoints, 
+			double discountRate, String studentIDNum) {
+		this.username = username;
+		this.password = password;
+		this.email = email;
+		this.wallet = wallet;
+		this.tickets = tickets;
+		this.cart =  cart;
+		this.age = age;
+		this.rewardPoints = rewardPoints;
+		this.discountRate = discountRate;
+		this.studentIDNum = studentIDNum;
+	}
+
+}
