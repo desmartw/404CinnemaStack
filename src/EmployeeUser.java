e711dbf7a93990ac5161b62665c390bf12589660
Wils DesMarteau <wilsdesmarteau@Wilss-MacBook-Pro.local>
2020-04-05 22:24:06 -0400

    Made the rest of user subclasses, updated user

---
+++ src/EmployeeUser.java
@@ -0,0 +1,35 @@
+
+
+public class EmployeeUser extends User {
+	public String employeeID;
+	
+	public EmployeeUser() {
+		username = "employedBob123";
+		password = "password";
+		email = "employedBob123@email.com";
+		wallet = new Currency[3];
+		tickets = new ArrayList<Ticket>();
+		cart =  new ArrayList<Ticket>();
+		age = 18;
+		rewardPoints = 0;
+		discountRate = .1;
+		employeeID = "123LoveToWork";
+	}
+	
+	public EmployeeUser(String username, String password, String email,
+			Currency[] wallet, ArrayList<Ticket> tickets,
+			ArrayList<Ticket> cart, int age, int rewardPoints, 
+			double discountRate, String employeeID) {
+		this.username = username;
+		this.password = password;
+		this.email = email;
+		this.wallet = wallet;
+		this.tickets = tickets;
+		this.cart =  cart;
+		this.age = age;
+		this.rewardPoints = rewardPoints;
+		this.discountRate = discountRate;
+		this.employeeID = employeeID;
+	}
+
+}
