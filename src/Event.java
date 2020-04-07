e2f6d05e4a43d8cf0a567187028049edc3dce2bf
Shayon Ghoshroy <shayonghoshroy@Shayons-MacBook-Pro.local>
2020-04-06 22:45:20 -0400

    fixed beginning of event class

--- src/Event.java
+++ src/Event.java
@@ -4,52 +4,124 @@
  * Class: Event
  *
  */
-import java.util.ArrayList; //Importing arraylist class
+import java.util.ArrayList;
+
 public class Event {
 	private String name;
-	private String location;
-	private int militaryTime;
-	private String genre;
-	private ArrayList<String> actors;
+	private ArrayList<Integer> militaryTimes;
+	private String type;
+	private int rating = 0;
+	private int numOfRatings = 0;
+	private ArrayList<String> comments;
+	private ArrayList<String> dates; //MMDDYYYY
+	private Double price;
 	/**
 	 * Default constructor
 	 */
 	public Event() {
-		name = "mars movies";
-		location = "mars";
-		militaryTime = 1100;
-		genre = "Comedy";
-		actors = new ArrayList<String>();
+		name = "";
+		militaryTimes = new ArrayList<Integer>();
+		type = "";
+		rating = 0;
+		numOfRatings = 0;
+		comments = new ArrayList<String>();
+		dates = new ArrayList<String>();
+		price = 0.0;
 	}
 	/**
 	 * Parameterized constructor
 	 * @param name
-	 * @param location
-	 * @param militaryTime
-	 * @param genre
-	 * @param actors
+	 * @param militaryTimes
+	 * @param type
+	 * @param rating
+	 * @param numOfRatings
+	 * @param comments
+	 * @param dates
+	 * @param price
 	 */
-	public Event(String name, String location,
-			int militaryTime, String genre, 
-			ArrayList<String> actors) {
+	public Event(String name, ArrayList<Integer> militaryTimes, String type, int rating, 
+			int numOfRatings, ArrayList<String> comments, ArrayList<String> dates, 
+			Double price) {
 		this.name = name;
-		this.location = location;
-		this.militaryTime = militaryTime;
-		this.genre = genre;
-		this.actors = actors;
+		this.militaryTimes = militaryTimes;
+		this.type = type;
+		this.rating = rating;
+		this.numOfRatings = numOfRatings;
+		this.comments = comments;
+		this.dates = dates;
+		this.price = price;
 	}
 	/**
-	 * ToString method returns all attributes of 
-	 * event 
+	 * printEvent method returns all attributes of event 
 	 */
-	public String toString() {
-		return("Name of event: "+ name +
-				"Location: "+ location+
-				"Time (24hr): "+ militaryTime +
-				"Genere: "+ genre +
-				"Actors: "+ actors);
+	public void printEvent() {
+		System.out.println("\nName of event: "+ name);
+		System.out.print("Times: | ");
+		militaryTimes.forEach(militaryTime ->{System.out.print(militaryTime + " | ");});
+		System.out.println("\nType: " + type);
+		System.out.println("Rating: " + rating);
+		System.out.println("Number of ratings: " + numOfRatings);
+		System.out.println("Rating: " + rating);
+		System.out.print("Comments: | ");
+		comments.forEach(comment ->{System.out.print(comment + " | ");});
+		System.out.println("\nDates: | ");
+		dates.forEach(date ->{System.out.print(date + " | ");});
+		System.out.println("\nPrice: " + price);
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public ArrayList<Integer> getMilitaryTimes() {
+		return militaryTimes;
+	}
+	public String printMilitaryTimes() {
+		return militaryTimes;
+	}
+	public void setMilitaryTimes(ArrayList<Integer> militaryTimes) {
+		this.militaryTimes = militaryTimes;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public int getRating() {
+		return rating;
+	}
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+	public int getNumOfRatings() {
+		return numOfRatings;
+	}
+	public void setNumOfRatings(int numOfRatings) {
+		this.numOfRatings = numOfRatings;
+	}
+	public ArrayList<String> getComments() {
+		return comments;
+	}
+	public void setComments(ArrayList<String> comments) {
+		this.comments = comments;
+	}
+	public ArrayList<String> getDates() {
+		return dates;
+	}
+	public void setDates(ArrayList<String> dates) {
+		this.dates = dates;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
 	}
 	//TODO add notifications
+	/*
 	public String getName() {
 		return name;
 	}
@@ -80,6 +152,7 @@ public class Event {
 	public void setActors(ArrayList<String> actors) {
 		this.actors = actors;
 	}
+	*/
 	
 	
 
--- src/EventDatabase.java
+++ src/EventDatabase.java
@@ -1,3 +1,20 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import javax.swing.JPasswordField;
+import javax.swing.JOptionPane;
+
+import java.io.FileWriter;
+
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class EventDatabase {
 
