1a758ce4d237339112059c027180af1a86bb508f
Wils DesMarteau <wilsdesmarteau@Wilss-MacBook-Pro.local>
2020-04-09 15:36:14 -0400

    validation for lDB

--- src/LocationDatabase.java
+++ src/LocationDatabase.java
@@ -39,14 +39,28 @@ public class LocationDatabase {
 	        System.out.println();
 	        String name = validateName();
 	        String description = validateDescription();
-	        int seatNumber = validateSeatNumber();
-	        ArrayList<Event> events = validateEvents();
+	        String password = validatePassword();
+	        int seatingRows = validateSeatingRows();
+	        int seatingCols = validateSeatingCols();
+	        int handicapStartSeat = validateHandicapStartSeat();
+	        //checks that there are hanciapped seats 
+	        if(handicapStartSeat !=  -1) {
+	        	int handicapEndSeat = validateHandicapEndSeat();
+	        } else {
+	        	int handicapEndSeat = 0;
+	        }
+	        ArrayList<Showtime> showtimes = new ArrayList<Showtime>();
 	        
-	        JSONObject locationDetails = new JSONObject();
+	        org.json.JSONObject locationDetails = new JSONObject();
 	        
 	        locationDetails.put("Name",  name);
-	        locationDetails.put("SeatNumber", seatNumber);
-	        locationDetails.put("Events", events);
+	        locationDetails.put("Description", description);
+	        locationDetails.put("Password", password);
+	        locationDetails.put("SeatRows", seatingRows);
+	        locationDetails.put("SeatCols", seatingCols);
+	        locationDetails.put("HandicapStartSeat", handicapStartSeat);
+	        locationDetails.put("HandicapEndSeat", handicapStartSeat);
+	        locationDetails.put("Showtimes", showtimes);
 	        
 	        JSONObject locationObject = new JSONObject();
 	        locationObject.put("Location", locationDetails);
@@ -54,7 +68,65 @@ public class LocationDatabase {
 	        locationList = readLocationList();
 	        locationList.add(locationObject);
 	    }
-	    /**
+	    
+	    private int validateHandicapStartSeat() {
+			// TODO Auto-generated method stub
+	    	int hStartSeat = 0;
+	    	System.out.println("If you would like to add handicapped seats, enter a starting seat number (between 1-5). \n"
+	    			+ "If not, hit 0.");
+	    	hStartSeat = scan.nextInt();
+	    	if(hStartSeat == 0) {
+	    		return -1;
+	    	} else if(hStartSeat > 1 && hStartSeat <= 5) {
+	    		return hStartSeat;
+	    	}
+			return hStartSeat;
+		}
+		private int validateHandicapEndSeat() {
+			int hEndSeat = 0;
+			System.out.println("Where would you like the handicapped seating to stop? enter a seat number (between 1-5). \n");
+			while(true) {
+			hEndSeat = scan.nextInt();
+			if(hEndSeat < 1 || hEndSeat >= 5) {
+				System.out.println("Invalid number, try again");
+			} else {
+				System.out.println("End seat added");
+				
+			}
+			break;
+			}
+			return hEndSeat;
+		}
+		
+		private int validateSeatingCols() {
+			 int seatCols = 0;
+		        while(true) {
+		        	System.out.println("Enter the seat col you would like in range 1-25: ");
+		        	seatCols = scan.nextInt();
+		        	if(seatCols == 0 || seatCols> 25) {
+		        		System.out.println("Your seat col number is out of range");
+		        		continue;
+		        	}
+		        	System.out.println("number of Cols added");
+		        	break;
+		        }
+		        return seatCols;
+		}
+		private String validatePassword() {
+			// TODO Auto-generated method stub
+			String pwd;
+			while(true) {
+	        	System.out.println("Enter your desired password, between 3-10 char: ");
+	        	pwd = scan.nextLine();
+	        	if(pwd.length() == 0 || pwd.length() > 10) {
+	        		System.out.println("Your password is too long or too short, try again");
+	        	} 
+	        	System.out.println("Password saved.");
+	        	break;
+		}
+		  	return pwd;
+		}
+		/**
 	     * returns a JSONarray of locations
 	     * @return
 	     */
@@ -99,19 +171,19 @@ public class LocationDatabase {
 	     *  Makes sure seatNumber of location is valid
 	     * @return
 	     */
-	    public int validateSeatNumber() {
-	        int seatNum = 0;
+	    public int validateSeatingRows() {
+	        int seatRows = 0;
 	        while(true) {
-	        	System.out.println("Enter the seat number you would like in range 1-25: ");
-	        	seatNum = scan.nextInt();
-	        	if(seatNum == 0 || seatNum > 25) {
-	        		System.out.println("Your seat selection is out of range");
+	        	System.out.println("Enter the seat rows you would like in range 1-25: ");
+	        	seatRows = scan.nextInt();
+	        	if(seatRows == 0 || seatRows> 25) {
+	        		System.out.println("Your seat row number is out of range");
 	        		continue;
 	        	}
-	        	System.out.println("Seat selected");
+	        	System.out.println("number of rows added");
 	        	break;
 	        }
-	        return seatNum;
+	        return seatRows;
 	    }
 	    /**
 	     * Makes sure description of location is valid
@@ -132,13 +204,7 @@ public class LocationDatabase {
 	    		}
 	    	return description;
 	    }
-	    	
-	
-	    public ArrayList<Event> validateEvents() {
-	        //TODO validate event
-	    	
-	    	return null;
-	    }
+	    
 	    
 	    /**
 	     * reads and prints locations to console
