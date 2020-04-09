c93e1315ba09cea7b2b7265f3f37be5887bb629a
RHenderzon <rrh3@email.sc.edu>
2020-04-09 19:26:49 -0400

    Driver.java and Page.java updates

--- src/Driver.java
+++ src/Driver.java
@@ -6,14 +6,59 @@ public class Driver {
 	public static User user;
 	private static Scanner in;
 	
-	// all the services that the program will offer, to be used in different functions
-	public enum Service {EXIT, SEARCH, LOGIN, VIEW_POPULAR, VIEW_ACCOUNT, SEARCH_BY_NAME,
-				  		 SEARCH_BY_DATE, SEARCH_BY_RATING, BUY_TICKETS, VIEW_CART, 
-				  		 EMPTY_CART, RENEW_FRONT, RENEW_CHOICE, GO_BACK, ADD_EVENT,
-				  		 ADD_LOCATION, ADD_USER, SELECT_TICKET, RATE_EVENT}; 
+	// Page construction
+	Page homePage = new Page();
 	
-	// holds the choice history of the user
-	private static ArrayList<Service> choiceHistory = new ArrayList<Service>();
+	// search pages
+	Page searchPage = new Page();
+	Page searchByTitlePage = new Page();
+	Page searchByDatePage = new Page();
+	Page searchByRatingPage = new Page();
+	Page searchByActorPage = new Page();
+	
+	Page accountPage = new Page();
+	Page eventsPage = new Page();
+	
+	// Page setup and initialization
+	// homepage setup display and 
+	public void setUpHomePage() {
+		homePage.setDisplay("  *******************    ******************* \n" +
+				 		 	"  *                 *    *                 * \n" +
+				 		 	"  *    HOME PAGE    *    *    0: Search    * \n" +
+				 		 	"  *                 *    *                 * \n" +
+				 		 	"  *******************    ******************* \n" +
+				 		 	"                                             \n" +
+				 		 	"  *******************    ******************* \n" +
+				 		 	"  *                 *    *       View      * \n" +
+				 		 	"  * 1: View Account *    *   2: Popular    * \n" +
+				 		 	"  *                 *    *      Events     * \n" +
+				 		 	"  *******************    ******************* \n" +
+				 		 	homePage.goToAdminOptions(user) + homePage.goToEmployeeOptions(user) + "\n");
+		homePage.getLinks().add(searchPage);
+		homePage.getLinks().add(accountPage);
+		homePage.getLinks().add(eventsPage);
+	}
+	
+	// searchpage setup display
+	public void setUpSearchPage() {
+		searchPage.setDisplay("  *******************    *******************    ******************* \n" +
+	 		 				  "  *                 *    *                 *    *				 * \n" +
+	 		 				  "  *   Search Page   *    *   0: By Title   *    *   1: By Date	 * \n" +
+	 		 				  "  *                 *    *                 *    *				 * \n" +
+	 		 				  "  *******************    *******************    ******************* \n" +
+	 		 				  "                                             					   \n" +
+	 		 				  "  *******************    *******************    ******************* \n" +
+	 		 				  "  *                 *    * 			      *    *				 * \n" +
+	 		 				  "  *   2: By Rating  *    *   3: By Actor   *    *  4: Home Page	 * \n" +
+	 		 				  "  *                 *    * 			      *    *				 * \n" +
+	 		 				  "  *******************    *******************    ******************* \n\n" +
+	 		 				  "  Pick the number for the criteria you wish to search by: " +
+	 		 				  homePage.goToAdminOptions(user) + homePage.goToEmployeeOptions(user) + "\n");
+		homePage.getLinks().add(searchByTitlePage);
+		homePage.getLinks().add(searchByDatePage);
+		homePage.getLinks().add(searchByRatingPage);
+		homePage.getLinks().add(searchByActorPage);
+	}
 	
 	// welcomes the user with greeting messages
 	private static void welcomeUser() {
@@ -23,302 +68,9 @@ public class Driver {
 		System.out.println("~~~ Please enjoy the fine services we have to offer ~~~\n");
 	}
 	
-	// displays a farewell upon exiting the application
-	private static void displayFarewell() {
-		// TODO add cool farewell with dope text image
-	}
-	
-	// Gives the user actions to take upon entering the app, or when the loop
-	// starts over because they go back to the initial actions
-	private static Service getTopChoice() {
-		System.out.println("Please enter one of the numbers below corresponding to the action you wish to take...");
-		if (user.getType().equalsIgnoreCase("guest")) {
-			return getUserChoice(new Service[] {Service.EXIT, Service.LOGIN, Service.SEARCH, Service.VIEW_POPULAR});
-		} else if (user.getType().equalsIgnoreCase("employee")) {
-			return getUserChoice(new Service[] {Service.EXIT, Service.VIEW_ACCOUNT, Service.SEARCH, Service.VIEW_POPULAR,
-												Service.ADD_EVENT});
-		} else if (user.getType().equalsIgnoreCase("admin")) {
-			return getUserChoice(new Service[] {Service.EXIT, Service.VIEW_ACCOUNT, Service.SEARCH, Service.VIEW_POPULAR,
-												Service.ADD_EVENT, Service.ADD_LOCATION, Service.ADD_USER});
-		} else {
-			return getUserChoice(new Service[] {Service.EXIT, Service.VIEW_ACCOUNT, Service.SEARCH, Service.VIEW_POPULAR});
-		}
-	}
-	
-	// helper method to get user input with a set of service options easier
-	private static Service getUserChoice(Service[] options) {
-		
-		// present the options
-		for (int i = 0; i < options.length; i++) {
-			System.out.println("~ " + i + " " + getServiceMessage(options[i]));
-		}
-		
-		// capture user choice
-		int intChoice = getValidatedChoice(options.length);
-		
-		// return the chosen service
-		return options[intChoice];
-	}
-	
-	// helper method that validates user has entered in an integer corresponding to options presented
-	public static int getValidatedChoice(int numOptions) {
-		String choice = "";
-		int intChoice = -1;
-		while(true) {
-			choice = in.nextLine();
-			// validate choice is an integer
-			if (!(choice.matches("[0-9]+") || choice.length() == 0)) {
-				System.out.println("Choice must be an integer");
-				continue;
-			}
-			intChoice = Integer.parseInt(choice);
-			if (intChoice < 0 || intChoice >= numOptions) {
-				System.out.println("Choice must be an integer corresponding to an action");
-				continue;
-			}
-			break;
-		}
-		return intChoice;
-	}
-	
-	// helper method to return a message for each Service
-	private static String getServiceMessage(Service service) {
-		
-		// translate service to corresponding message
-				switch(service) {
-					case RENEW_FRONT:
-						return "Go to the front page";
-					case EXIT:
-						return "Exit the program";
-					case SEARCH:
-						return "Search for events";
-					case LOGIN:
-						return "Login or sign up for an account";
-					case VIEW_POPULAR:
-						return "View popular events";
-					case VIEW_ACCOUNT:
-						return "View your account information";
-					case SEARCH_BY_NAME:
-						return "Search for events by movie title";
-					case SEARCH_BY_DATE:
-						return "Search for events within a date range";
-					case SEARCH_BY_RATING:
-						return "Search for events by rating";
-					case BUY_TICKETS:
-						return "Purchase the tickets in your cart";
-					case VIEW_CART:
-						return "View the current tickets in your shopping cart";
-					case EMPTY_CART:
-						return "Empty your current shopping cart";
-					case GO_BACK:
-						return "Go back to previous page";
-					case ADD_EVENT:
-						return "Add an event to the database";
-					case SELECT_TICKET:
-						return "Add an event from the list to your shopping cart";
-					case RATE_EVENT:
-						return "Rate an event from the list";
-						default:
-							return "An invalid Service has been used";
-							
-				}
-	}
-	
-	// checks what choice the user made and acts appropriately
-	private static void actOnChoice(Service choice) {
-		
-		// create choice history or add new choice to the current history
-		addChoiceToHistory(choice);
-		
-		// translate choice to corresponding action
-		// all choices that do not lead to more options are followed by goBackChoice()
-		switch(choice) {
-			case RENEW_FRONT:
-				Service aChoice = getTopChoice();
-				actOnChoice(aChoice);
-				break;
-			case EXIT:
-				displayFarewell();
-				System.exit(0);
-				break;
-			case SEARCH:
-				initiateSearch();
-				break;
-			case LOGIN:
-				user = GenerateUser.generateUser();
-				goBackChoice();
-				break;
-			case VIEW_POPULAR:
-				viewPopularEvents();
-				break;
-			case VIEW_ACCOUNT:
-				user.viewAccount();
-				Service vaChoice = getUserChoice(new Service[] {Service.EXIT, Service.BUY_TICKETS,Service.VIEW_CART, Service.EMPTY_CART, Service.GO_BACK, Service.RENEW_FRONT});
-				actOnChoice(vaChoice);
-				break;
-			case SEARCH_BY_NAME:
-				searchByName();
-				break;
-			case SEARCH_BY_DATE:
-				searchByDateRange();
-				break;
-			case SEARCH_BY_RATING:
-				searchByRating();
-				break;
-			case BUY_TICKETS:
-				user.purchaseTickets();
-				goBackChoice();
-				break;
-			case VIEW_CART:
-				user.viewCart();
-				Service vcChoice = getUserChoice(new Service[] {Service.EXIT, Service.BUY_TICKETS, Service.EMPTY_CART, Service.GO_BACK, Service.RENEW_FRONT});
-				actOnChoice(vcChoice);
-				break;
-			case EMPTY_CART:
-				user.emptyCart();
-				System.out.println("Cart emptied ... reloading previous page ...");
-				goBackChoice();
-				break;
-			case RENEW_CHOICE:
-				// remove the choice taken to RENEW_CHOICE
-				choiceHistory.remove(choiceHistory.size()-1);
-				renewChoice();
-				break;
-			case GO_BACK:
-				// remove the choice taken to GO_BACK
-				choiceHistory.remove(choiceHistory.size()-1);
-				goBackChoice();
-				break;
-			case ADD_EVENT:
-				enterEventToLocation();
-				Service aeChoice = getTopChoice();
-				actOnChoice(aeChoice);
-				break;
-			case ADD_LOCATION:
-				// TODO
-				break;
-			case ADD_USER:
-				// TODO
-				break;
-			case SELECT_TICKET:
-				// TODO
-				break;
-			case RATE_EVENT:
-				DatabaseDriver.enterRating();
-				break;
-			// other functions should be set up so that this won't occur but 
-			// any improper case will just go back
-				default:
-					goBackChoice();
-		}
-	}
-	
-	/** adds the given choice to the user's choice history 
-	 *  @param choice - Service choice to be added on top of the history stack
-	 */
-	private static void addChoiceToHistory(Service choice) {
-		choiceHistory.add(choice);
-	}
-	
-	// deletes the latest choice and renews the previous choice
-	private static void goBackChoice() {
-		
-		// check if at least two choices have been made
-		if (choiceHistory.size() > 1) {
-			// remove the latest choice and renew the choice
-			choiceHistory.remove(choiceHistory.size()-1);
-			renewChoice();
-		} else if (choiceHistory.size() == 1) {
-			// renew the choice
-			renewChoice();
-		} else {
-			// choiceHistory shouldn't be empty but show top choices if so
-			Service choice = getTopChoice();
-			actOnChoice(choice);
-		}
-	}
-	
-	// re-calls the function associated to the latest choice, if none, go through the loop again
-	private static void renewChoice() {
-		
-		// check if a choice has been made
-		if (!choiceHistory.isEmpty()) {
-			// save the latest choice
-			Service latestChoice = choiceHistory.get(choiceHistory.size()-1);
-			// delete the latest choice from history since actOnChoice will add it to the history
-			choiceHistory.remove(choiceHistory.size()-1);
-			actOnChoice(latestChoice);
-		} else {
-			// call top options if no choice has been made - should never happen
-			Service choice = getTopChoice();
-			actOnChoice(choice);
-		}
-	}
-	
-	//****************** FUNCTIONS CALLED AFTER DISPLAYING AN EVENT TO SCREEN *******//
-	
-	//******************* FUNCTIONS CALLED AFTER SELECTING SEARCH ********************//
-	
-	private static void initiateSearch() {
-		System.out.println("Select the criteria you wish to search by: \n");
-		Service choice = getUserChoice(new Service[] {Service.EXIT, Service.SEARCH_BY_DATE, Service.SEARCH_BY_NAME,
-													  Service.SEARCH_BY_RATING, Service.GO_BACK});
-		actOnChoice(choice);
-	}
-	
-	private static void searchByName() {
-		System.out.println("Enter the event name you wish to search: ");
-		String name = in.nextLine();
-		Event event = DatabaseDriver.returnEvent(name);
-		if (event == null) {
-			System.out.println("No event by that name found ... reloading Search page");
-			goBackChoice();
-		} else {
-			System.out.println("Event found ... loading Event information ...\n");
-			event.printEvent();
-		}
-	}
-	
-	private static void searchByDateRange() {
-		// TODO have user enter two dates
-		// TODO search database for events within those dates
-		// TODO give results in chronological order, same time events are given by rating
-		// TODO give next options
-	}
-	
-	private static void searchByDateRange(Event event) {
-		// TODO have user enter two dates
-		// TODO search database for events within those dates
-		// TODO give results in chronological order, same time events are given by rating
-		// TODO give next options
-	}
-	
-	private static void searchByRating() {
-		// TODO have user enter a rating
-		// TODO get all results above or equal to entered rating
-		// TODO display all the Events given a certain rating
-		// TODO display next option e.g choose an event and see its schedule, go back, go to beginning
-	}
-	
-	//******************* FUNCTIONS CALLED AFTER SELECTING VIEW_POPULAR ********************//
-	
-	private static void viewPopularEvents() {
-		// TODO access ratings to show the top events
-		// TODO display the top events
-		// TODO ask next options
-		//(DatabaseDriver.returnEvent("Frozen 2")).printEvent();
-		DatabaseDriver.displayAllEvents();
-		Service choice = getUserChoice(new Service[] {Service.EXIT, Service.SELECT_TICKET, Service.RATE_EVENT, Service.GO_BACK});
-		actOnChoice(choice);
-	}
-	
-  private static void enterEventToLocation() {
-		System.out.println("Enter the name of the location for the event:");
-		String loc = in.nextLine();
-		DatabaseDriver.enterNewEventWithLocation(loc);
-	}
-	
 	public static void main(String[] args) {
+		
+		
 		// create input stream
 		in = new Scanner(System.in);
 		
@@ -327,10 +79,11 @@ public class Driver {
 		
 		// get the user to login or continue as guest
 		user = GenerateUser.generateUser();
-			
-		// request a service from the user
-		Service choice = getTopChoice();
-		actOnChoice(choice);
+		
+		if (!user.getType().equalsIgnoreCase("admin") && !user.getType().equalsIgnoreCase("employee")) {
+			System.out.println("Hello " + user.getUsername() + " please enter the number of your next move:");
+			System.out.println("1: View Account")
+		}
 		
 		// close input stream
 		in.close();
