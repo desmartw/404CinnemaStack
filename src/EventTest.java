72533595f29665ed86fc054318242b79812091c2
Wils DesMarteau <wilsdesmarteau@Wilss-MacBook-Pro.local>
2020-04-13 21:52:35 -0400

    event tests2

--- src/EventTest.java
+++ src/EventTest.java
@@ -19,6 +19,32 @@ public class EventTest {
 		Event event = new Event(name, type, comments, actors);
 		assertNotNull(event);
 	}
+	public void testTwoEventsArentEqual() {
+		String name = "Frozen";
+		String type = "movie";
+		ArrayList<String> comments = new ArrayList<String>();
+		comments.add("good film");
+		ArrayList<String> actors = new ArrayList<String>();
+		actors.add("brad pitt");
+		Event event = new Event(name, type, comments, actors);
+		String name2 = "Frozen";
+		String type2 = "movie";
+		ArrayList<String> comments2 = new ArrayList<String>();
+		comments.add("hated it");
+		ArrayList<String> actors2 = new ArrayList<String>();
+		actors.add("Anna");
+		actors.add("Elsa");
+		Event event2 = new Event(name2, type2, comments2, actors2);
+		assertNotEquals(event, event2);
+	}
+	public void testCreateNullEvent() {
+		String name = null;
+		String type = null;
+		ArrayList<String> comments = null;
+		ArrayList<String> actors = null;
+		Event event = new Event(name, type, comments, actors);
+		assertNull(event);
+	}
 	
 	public void testGetName() {
 		String name = "Frozen";
